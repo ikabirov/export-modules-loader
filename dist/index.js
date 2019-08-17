5a9bac8b52fc998525fc2b7c2ae224a5113a198c
Ilyas Kabirov <ilyas.kabirov@gmail.com>
2019-08-17 23:40:22 +0300

    output was moved to /generated

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 var path = require("path");
 
 module.exports = function(content) {
-  var outPath = path.resolve(__dirname, this.resourcePath).slice(this.rootContext.length)
+  var outPath = path.join('../generated', this.resourcePath.slice(this.rootContext.length))
   if (!outPath.endsWith('.js'))
   {
     outPath += '.js';
